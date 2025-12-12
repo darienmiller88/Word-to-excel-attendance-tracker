b41fc28e28c32d2b527c6eaca54c302187751770
Darien Miller <darienmiller88@yahoo.com>
2025-12-12 13:08:40 -0500

    feat: added license, and api key

--- main.go
+++ main.go
@@ -2,9 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/joho/godotenv"
+	"github.com/unidoc/unioffice/v2/common/license"
+	"github.com/unidoc/unioffice/v2/document"
 )
 
 func main(){
-	fmt.Println("hello world")
+	godotenv.Load()
+
+	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
+	
+	if err != nil {
+		panic(err)
+	}
+
+	doc, err := document.Open("./docs/Pelham/October/10_13_25 Pelham.docx")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("notes:", doc.ExtractText().Text())
 }
